de7e51fcbee939225a8ef5a8117ee29fc2652d95
Manba Osamu <manba@zf7.so-net.ne.jp>
2019-12-12 01:31:07 +0900

    トピック情報の保存場所をCookieからlocalStorage(store)に変更

--- src/js/utils/cookie.js
+++ src/js/utils/cookie.js
@@ -1,26 +1,13 @@
-var Cookie = require('cookie');
-var assign = require('object-assign');
-
-var nextMonth = function(date) {
-  date.setDate( date.getDate() + 30 );
-  return date;
-}
-
-// デフォルトの有効期限を30日間にする
-var defaultOpts = {
-  expires: nextMonth(new Date())
-};
+var store = require('store');
 
 module.exports = {
 
   set: function(name, str, opts) {
-    opts = assign(defaultOpts, opts);
-    document.cookie = Cookie.serialize(name, str, opts);
+    store.set(name, str);
   },
 
   get: function(name) {
-    var cookies = Cookie.parse(document.cookie);
-    return ( name === undefined ) ? cookies : cookies[name];
+    return store.get(name);
   }
 
 };
