73fe0c2f9c300a69650190cf2bc4a1434e530d70
Manba Osamu <manba@zf7.so-net.ne.jp>
2019-12-13 02:09:34 +0900

    ドキュメント類を更新

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# [Topic Timer](http://hotolab.net/TopicTimer/)
+# Topic Timer
 
 ミーティングやプレゼンテーションでのタイムキーピングのためのアプリです。
 決められた時間通りに物事を進めたいという場合にご活用ください。
--- src/js/components/Info.jsx
+++ src/js/components/Info.jsx
@@ -17,8 +17,10 @@ module.exports = React.createClass({
           <h3>お問い合わせ</h3>
           <p>
             不具合・改善要望などありましたら、
-            <a href='https://github.com/hoto17296/TopicTimer' target='_blank'>GitHub</a> にプルリクエストを投げるか、
-            <a href='http://hoto.me/' target='_blank'>@hoto17296</a> までご連絡ください。
+            <a href='https://github.com/manba036/TopicTimer' target='_blank'>GitHub</a> にプルリクエストを投げてください。
+          </p>
+          <p>
+            本家は<a href='https://github.com/hoto17296/TopicTimer' target='_blank'>こちら</a>です。
           </p>
         </div>
       </Page>
